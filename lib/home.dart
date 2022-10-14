18555e881524b3a466e4da9f12cd82590d8b9baf
rdwayne876 <63379431+rdwayne876@users.noreply.github.com>
2022-10-13 23:05:46 -0500

    Home screen layout

--- lib/home.dart
+++ lib/home.dart
@@ -11,6 +11,29 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: Column( crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 50),
+            Text( 'Amber Heart',
+            style: TextStyle( color: Colors.deepOrange,
+              fontSize: 20),
+            ),
+            SizedBox( height: 5),
+            Text( 'Cat/Dog Detector App',
+            style: TextStyle(
+                color: Colors.deepOrange,
+              fontWeight: FontWeight.w500,
+              fontSize: 30
+            ),
+            ),
+            SizedBox( height: 50),
+          ],
+        )
+      ),
+    );
   }
 }
